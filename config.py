f7089d91369aa420d69510b562b43f1a72fdb078
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-12-06 13:08:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "27060846")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "8f39072a61dbb296f38e4ff2b6cbe478") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6712788582:AAHKWFe3pcKMbnDJXiaLVa6IjXG2lddJin0") # ⚠️ Required
    
     # database config
-    DB_URL  = os.environ.get("DB_URL","")  # ⚠️ Required
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://TEST:TEST@cluster0.ehigmtx.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
     DB_NAME  = os.environ.get("DB_NAME","AutoAcceptBot")
  
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/0ceb5f176f3cf877a08b5.jpg")
-    ADMIN       = int(os.environ.get('ADMIN', '')) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "")) # ⚠️ Required
+    ADMIN       = int(os.environ.get('ADMIN', '6065594762')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001971176803")) # ⚠️ Required
     WELCOME_MSG = os.environ.get("WELCOME_MSG", "Hey {user},\nYour Request Approved ✅,\n\nWelcome to **{title}**")
     LEAVE_MSG = os.environ.get("LEAVE_MSG", "By {user},\nSee You Again 👋\n\nFrom **{title}**")
     
