cb152869c97c5fe042f39655fdd8986d78af371a
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-02-08 12:39:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,3 +23,23 @@ class Config(object):
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8080"))
+
+
+class TxT(object):
+
+    HELP_MSG = """
+<b> ADMIN Available commands:- </b>
+
+➜ /set_welcome - To set custom welcome message (support photo & video & animation or gif)
+➜ /set_leave - To set custom leave message (support photo & video & animation or gif)
+➜ /option - To toggle your welcome & leave message (whether it'll show to user or not)
+➜ /status - To see status about bot
+➜ /restart - To restart the bot
+➜ /broadcast - To brodcast the users (only those user who has started your bot)
+
+
+⚠️ <b> Support HTML & Markdown formating in welcome or leave message for more info <a href=https://core.telegram.org/api/entities#:~:text=%2C%20MadelineProto.-,Allowed%20entities,-For%20example%20the> Link </a>. </b>
+
+
+<b>⦿ Developer:</b> <a href=https://t.me/Snowball_Official>ѕησωвαℓℓ ❄️</a>
+"""
