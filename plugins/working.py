0ac93f3ea15924cf07685e6cc38c48db60568021
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-02-08 12:40:01 +0530

    Update working.py

--- plugins/working.py
+++ plugins/working.py
@@ -1,14 +1,17 @@
 import sys
+import logging
+import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import ChatMemberUpdated, ChatJoinRequest
 from config import Config
 from helper.database import db
+from pyrogram.errors import FloodWait
+
+logging.basicConfig(level=logging.ERROR)
 
 
-@Client.on_chat_join_request(filters.group | filters.channel)
-async def handle_autoAccept(bot: Client, message: ChatJoinRequest):
+async def approve_func(bot, message):
     try:
-
         chat = message.chat
         user = message.from_user
         await bot.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
@@ -17,15 +20,33 @@ async def handle_autoAccept(bot: Client, message: ChatJoinRequest):
 
         if bool_welcome:
             welcome_messagae = await db.get_welcome(Config.ADMIN)
-            if welcome_messagae:
-                await bot.send_message(chat_id=user.id, text=welcome_messagae.format(user=user.mention, title=chat.title))
+            photo_or_video_file = await db.get_welc_file(Config.ADMIN)
+            if photo_or_video_file:
+                try:
+                    try:
+                        await bot.send_photo(chat_id=user.id, photo=photo_or_video_file, caption=welcome_messagae.format(user=user.mention, title=chat.title) if welcome_messagae else Config.DEFAULT_WELCOME_MSG.format(user=user.mention, title=chat.title))
+                    except:
+                        await bot.send_animation(chat_id=user.id, animation=photo_or_video_file, caption=welcome_messagae.format(user=user.mention, title=chat.title) if welcome_messagae else Config.DEFAULT_WELCOME_MSG.format(user=user.mention, title=chat.title))
+                except:
+                    await bot.send_video(chat_id=user.id, video=photo_or_video_file, caption=welcome_messagae.format(user=user.mention, title=chat.title) if welcome_messagae else Config.DEFAULT_WELCOME_MSG.format(user=user.mention, title=chat.title))
 
             else:
-                await bot.send_message(chat_id=user.id, text=Config.WELCOME_MSG.format(user=user.mention, title=chat.title))
-                
+                await bot.send_message(chat_id=user.id, text=welcome_messagae.format(user=user.mention, title=chat.title) if welcome_messagae else Config.DEFAULT_WELCOME_MSG.format(user=user.mention, title=chat.title))
     except Exception as e:
-        print('Error on line {}'.format(
-            sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
+        pass
+        # logging.error(str(e))
+
+
+@Client.on_chat_join_request(filters.group | filters.channel)
+async def handle_autoAccept(bot: Client, message: ChatJoinRequest):
+    try:
+        while True:
+            await approve_func(bot, message)
+
+    except FloodWait as s:
+        asyncio.sleep(s.value)
+        while True:
+            await approve_func(bot, message)
 
 
 @Client.on_chat_member_updated()
@@ -38,11 +59,18 @@ async def handle_chat(bot: Client, update: ChatMemberUpdated):
 
             if bool_leave:
                 leave_message = await db.get_leave(Config.ADMIN)
-                if leave_message:
-                    await bot.send_message(chat_id=left_user.user.id, text=leave_message.format(user=left_user.user.mention, title=update.chat.title))
+                photo_or_video_file = await db.get_leav_file(Config.ADMIN)
+                if photo_or_video_file:
+                    try:
+                        try:
+                            await bot.send_photo(chat_id=left_user.user.id, photo=photo_or_video_file, caption=leave_message.format(user=left_user.user.mention, title=update.title) if leave_message else Config.DEFAULT_LEAVE_MSG.format(user=left_user.user.mention, title=update.chat.title))
+                        except:
+                            await bot.send_animation(chat_id=left_user.user.id, animation=photo_or_video_file, caption=leave_message.format(user=left_user.user.mention, title=update.chat.title) if leave_message else Config.DEFAULT_LEAVE_MSG.format(user=left_user.user.mention, title=update.chat.title))
+                    except:
+                        await bot.send_video(chat_id=left_user.user.id, video=photo_or_video_file, caption=leave_message.format(user=left_user.user.mention, title=update.chat.title) if leave_message else Config.DEFAULT_LEAVE_MSG.format(user=left_user.user.mention, title=update.chat.title))
 
-                else:
-                    await bot.send_message(chat_id=left_user.user.id, text=Config.LEAVE_MSG.format(user=left_user.user.mention, title=update.chat.title))
+            else:
+                await bot.send_message(chat_id=left_user.user.id, text=leave_message.format(user=left_user.user.mention, title=update.chat.title) if leave_message else Config.DEFAULT_LEAVE_MSG.format(user=left_user.user.mention, title=update.chat.title))
 
         except Exception as e:
             print('Error on line {}'.format(
