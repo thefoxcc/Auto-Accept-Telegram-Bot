22fb8fa06084d16d465f8edba42c479a8c25a800
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-02-11 18:45:41 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -18,35 +18,38 @@ class Database:
             welc_file=None,
             leav_file=None,
             welcome=None,
-            leave = None,
-            bool_welc = None,
-            bool_leav = None
+            leave=None,
+            bool_auto_accept=True,
+            bool_welc=None,
+            bool_leav=None,
+            channel=[]
         )
-    
+
     def new_user(self, id):
         return dict(
-            id = int(id),
+            id=int(id),
             join_date=datetime.datetime.today().isoformat()
         )
 
     def approved_user(self, id):
         return dict(
-            id = int(id)
+            id=int(id)
         )
+
     async def set_welcome(self, user_id, welcome):
         await self.col.update_one({'id': int(user_id)}, {'$set': {'welcome': welcome}})
 
     async def get_welcome(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('welcome', None)
-    
+
     async def set_welc_file(self, user_id, welc_file):
         await self.col.update_one({'id': int(user_id)}, {'$set': {'welc_file': welc_file}})
 
     async def get_welc_file(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('welc_file', None)
-    
+
     async def set_leav_file(self, user_id, leav_file):
         await self.col.update_one({'id': int(user_id)}, {'$set': {'leav_file': leav_file}})
 
@@ -60,6 +63,13 @@ class Database:
     async def get_leave(self, id):
         user = await self.col.find_one({'id': int(id)})
         return user.get('leave', None)
+
+    async def set_bool_auto_accept(self, user_id, bool_auto_accept):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'bool_auto_accept': bool_auto_accept}})
+
+    async def get_bool_auto_accept(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('bool_auto_accept', None)
 
     async def set_bool_welc(self, user_id, bool_welc):
         await self.col.update_one({'id': int(user_id)}, {'$set': {'bool_welc': bool_welc}})
@@ -75,6 +85,25 @@ class Database:
         user = await self.col.find_one({'id': int(id)})
         return user.get('bool_leav', None)
 
+    async def set_channel(self, user_id, channel_id):
+        user = await self.col.find_one({'id': int(user_id)})
+        if user:
+            channels = user.get('channel', [])
+            channels.append(channel_id)
+            await self.col.update_one({'id': int(user_id)}, {'$set': {'channel': channels}})
+
+    async def get_channel(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('channel', [])
+    
+    async def remove_channel(self, user_id, channel_id):
+        user = await self.col.find_one({'id': int(user_id)})
+        if user:
+            channels = user.get('channel', [])
+            if channel_id in channels:
+                channels.remove(channel_id)
+                await self.col.update_one({'id': int(user_id)}, {'$set': {'channel': channels}})
+
     async def add_user(self, b, m):
         u = m.from_user
         if not await self.is_user_exist(u.id):
@@ -82,7 +111,7 @@ class Database:
                 user = self.admin_user(u.id)
             else:
                 user = self.new_user(u.id)
-                
+
             await self.col.insert_one(user)
             await send_log(b, u)
 
@@ -92,7 +121,7 @@ class Database:
             user = self.approved_user(u.id)
             await self.col.insert_one(user)
             await send_log(b, u)
-            
+
     async def is_user_exist(self, id):
         user = await self.col.find_one({'id': int(id)})
         return bool(user)
