883681994e3568d488e5fc03764128da63d7deb9
thefoxcc <94108436+thefoxcc@users.noreply.github.com>
2024-02-13 22:50:18 +0400

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,13 +14,13 @@ from helper.utils import (
 
 @Client.on_message(filters.private & filters.command("start"))
 async def handle_start(bot: Client, message: Message):
-    SnowDev = await message.reply_text(text="**Please Wait...**", reply_to_message_id=message.id)
+    SnowDev = await message.reply_text(text="**Подождите, пожалуйста...**", reply_to_message_id=message.id)
     await db.add_user(b=bot, m=message)
-    text = f"Hi, {message.from_user.mention}\n\n I'm Auto Accept Bot I can accpet user from any channel and group just make me admin there."
+    text = f"Привет, {message.from_user.mention}\n\n Я Auto Accept Bot, я могу принимать пользователей из любого канала или группы, просто сделайте меня администратором там."
     reply_markup = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton(text="Developer 👨‍💻", url="https://t.me/Snowball_Official")],
-            [InlineKeyboardButton("Help", callback_data="help")],
+            [InlineKeyboardButton(text="Разработчик 👨‍💻", url="https://t.me/Snowball_Official")],
+            [InlineKeyboardButton("Помощь", callback_data="help")],
         ]
     )
     if Config.START_PIC:
@@ -29,19 +29,19 @@ async def handle_start(bot: Client, message: Message):
             await message.reply_photo(photo=Config.START_PIC, caption=text, reply_markup=reply_markup)
         else:
             await SnowDev.delete()
-            await message.reply_photo(photo=Config.START_PIC, caption=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Developer 👨‍💻", url="https://t.me/Snowball_Official")]]))
+            await message.reply_photo(photo=Config.START_PIC, caption=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Разработчик 👨‍💻", url="https://t.me/Snowball_Official")]]))
     else:
         if message.from_user.id == Config.ADMIN:
             await SnowDev.edit(text=text, reply_markup=reply_markup)
         else:
-            await SnowDev.edit(text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Developer 👨‍💻", url="https://t.me/Snowball_Official")]]))
+            await SnowDev.edit(text=text, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Разработчик 👨‍💻", url="https://t.me/Snowball_Official")]]))
 
 
 @Client.on_message(filters.private & filters.command("set_welcome") & filters.user(Config.ADMIN))
 async def set_welcome_msg(bot: Client, message: Message):
     welcome_msg = message.reply_to_message
     if welcome_msg:
-        SnowDev = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+        SnowDev = await message.reply_text("**Пожалуйста, подождите...**", reply_to_message_id=message.id)
         try:
             if welcome_msg.photo or welcome_msg.video or welcome_msg.animation:
                 await db.set_welcome(message.from_user.id, welcome_msg.caption)
@@ -51,16 +51,16 @@ async def set_welcome_msg(bot: Client, message: Message):
                 await db.set_welc_file(message.from_user.id, None)
         except Exception as e:
             return await SnowDev.edit(e)
-        await SnowDev.edit("Successfully Set Your Welcome Message ✅")
+        await SnowDev.edit("Сообщение приветствия успешно установлено ✅")
     else:
-        await message.reply_text("Invalid Command !\n⚠️ Format ➜ `Hey, {user} Welcome to {title}` \n\n **Reply to message**")
+        await message.reply_text("Недействительная команда !\n⚠️ Формат ➜ `Привет, {пользователь} Добро пожаловать в {название}` \n\n **Ответьте на сообщение**")
 
 
 @Client.on_message(filters.private & filters.command("set_leave") & filters.user(Config.ADMIN))
 async def set_leave_msg(bot: Client, message: Message):
     leave_msg = message.reply_to_message
     if leave_msg:
-        SnowDev = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+        SnowDev = await message.reply_text("**Пожалуйста, подождите...**", reply_to_message_id=message.id)
         try:
             if leave_msg.photo or leave_msg.video or leave_msg.animation:
                 await db.set_leave(message.from_user.id, leave_msg.caption)
@@ -70,14 +70,14 @@ async def set_leave_msg(bot: Client, message: Message):
                 await db.set_leav_file(message.from_user.id, None)
         except Exception as e:
             return await SnowDev.edit(e)
-        await SnowDev.edit("Successfully Set Your Leave Message ✅")
+        await SnowDev.edit("Сообщение об уходе успешно установлено ✅")
     else:
-        await message.reply_text("Invalid Command !\n⚠️ Format ➜ `Hey, {user} By See You Again from {title}` \n\n **Reply to message**")
+        await message.reply_text("Недействительная команда !\n⚠️ Формат ➜ `Привет, {пользователь} До свидания от {название}` \n\n **Ответьте на сообщение**")
 
 
 @Client.on_message(filters.private & filters.command('option') & filters.user(Config.ADMIN))
 async def set_bool_welc(bot: Client, message: Message):
-    SnowDev = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+    SnowDev = await message.reply_text("**Пожалуйста, подождите...**", reply_to_message_id=message.id)
 
     user_id = message.from_user.id
     bool_welc = await db.get_bool_welc(user_id)
@@ -88,18 +88,18 @@ async def set_bool_welc(bot: Client, message: Message):
     leav_buttons = [OnLeavBtn, OffLeavBtn]
     autoaccept_buttons = [OnAutoacceptBtn, OffAutoacceptBtn]
 
-    # Determine button configurations based on user settings
+    # Определение конфигураций кнопок на основе настроек пользователя
     welc_button_row = [welc_buttons[0] if bool_welc else welc_buttons[1],
                        leav_buttons[0] if bool_leav else leav_buttons[1]]
     autoaccept_button_row = [autoaccept_buttons[0]
                              if bool_auto_accept else autoaccept_buttons[1]]
 
-    # Update text and buttons based on user settings
-    text = "Click the button from below to toggle Welcome & Leaving Message also Auto Accept."
+    # Обновление текста и кнопок на основе настроек пользователя
+    text = "Нажмите кнопку ниже, чтобы переключить приветственные и прощальные сообщения, а также авто-принятие."
     reply_markup = InlineKeyboardMarkup(
         [welc_button_row, autoaccept_button_row])
 
-    # Edit message with updated text and buttons
+    # Редактирование сообщения с обновленным текстом и кнопками
     await SnowDev.edit(text=text, reply_markup=reply_markup)
 
 
@@ -109,7 +109,7 @@ async def handle_CallbackQuery(bot: Client, query: CallbackQuery):
     data = query.data
 
     if data.startswith('welc'):
-        text = "Click the button from below to toggle Welcome & Leaving Message also Auto Accept."
+        text = "Нажмите кнопку ниже, чтобы переключить приветственные и прощальные сообщения, а также авто-принятие."
         boolean = data.split('-')[1]
 
         if boolean == 'on':
@@ -141,7 +141,7 @@ async def handle_CallbackQuery(bot: Client, query: CallbackQuery):
                 return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OnWelcBtn, OffLeavBtn], [OffAutoacceptBtn]]))
 
     elif data.startswith('leav'):
-        text = "Click the button from below to toggle Welcome & Leaving Message also Auto Accept."
+        text = "Нажмите кнопку ниже, чтобы переключить приветственные и прощальные сообщения, а также авто-принятие."
         boolean = data.split('-')[1]
 
         if boolean == 'on':
@@ -162,15 +162,17 @@ async def handle_CallbackQuery(bot: Client, query: CallbackQuery):
             if await db.get_bool_welc(query.from_user.id):
                 if await db.get_bool_auto_accept(query.from_user.id):
                     return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OnWelcBtn, OnLeavBtn], [OnAutoacceptBtn]]))
+
                 return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OnWelcBtn, OnLeavBtn], [OffAutoacceptBtn]]))
 
             else:
                 if await db.get_bool_auto_accept(query.from_user.id):
-                    return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OffWelcBtn, OnLeavBtn], [OnAutoacceptBtn]]))
-                return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OffWelcBtn, OnLeavBtn], [OnAutoacceptBtn], [OffAutoacceptBtn]]))
+                    return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OnWelcBtn, OffLeavBtn], [OnAutoacceptBtn]]))
+
+                return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OffWelcBtn, OnLeavBtn], [OffAutoacceptBtn]]))
 
     elif data.startswith('autoaccept'):
-        text = "Click the button from below to toggle Welcome & Leaving Message also Auto Accept."
+        text = "Нажмите кнопку ниже, чтобы переключить приветственные и прощальные сообщения, а также авто-принятие."
         boolean = data.split('-')[1]
 
         if boolean == 'on':
@@ -213,7 +215,7 @@ async def handle_CallbackQuery(bot: Client, query: CallbackQuery):
                 return await query.message.edit(text=text, reply_markup=InlineKeyboardMarkup([[OffWelcBtn, OffLeavBtn], [OnAutoacceptBtn]]))
 
     elif data == 'help':
-        await query.message.edit(TxT.HELP_MSG, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('✘ Close ✘', callback_data='close')]]))
+        await query.message.edit(TxT.HELP_MSG, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('✘ Закрыть ✘', callback_data='close')]]))
 
     elif data == 'close':
         await query.message.delete()
